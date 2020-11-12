5750c5a5cf4bda3cbc83bc5966f00b73e6ff730b
igoryen <igor.yentaltsev@gmail.com>
2020-11-12 14:52:37 -0500

    Added vue-router plugin via Vue UI. Some files were auto-added.

---
+++ src/components/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <router-link to="/">Home</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({});
+</script>
