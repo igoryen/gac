da74220126f4b7da7412f14bfffe1c8f7f2ebb63
igoryen <igor.yentaltsev@gmail.com>
2021-01-26 18:12:03 -0500

    Created a new page for the Advanced CSS course.

---
+++ src/components/cssprojects/d210126.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="aaa">
+        <div>2021-01-26</div>
+
+        <header class="header">
+            Some text...
+        </header>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+
+// Light green: #7ed56f
+// Medium green: #55c57a
+// Dark green: #28b485
+</style>
---
+++ src/views/CSSprojects.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="css-projects">
+        hello
+        <D210126 />
+    </div>
+    
+</template>
+
+<script>
+import D210126 from "@/components/cssprojects/d210126.vue";
+export default {
+    name: "CSSprojects",
+    components: {
+        D210126
+    }
+}
+</script>
