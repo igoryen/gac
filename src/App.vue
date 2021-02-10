767156c8128537f3d557a81dffb297ef7e81f187
igoryen <igor.yentaltsev@gmail.com>
2021-02-09 19:54:48 -0500

    32. implemented 7-1 css architecture with SASS

--- src/App.vue
+++ src/App.vue
@@ -22,29 +22,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import '../src/assets/scss/main.scss';
-
-html {
-    font-size: 62.5%; // DR%
-    // DR% (the Design's Root font-size in percentage that must be typed in CSS)
-    // DRpx (the Design's Root font-size the design is based on, in pixels (e.g. 10px))
-    // Bpx (the Browser's font-size setting in pixels 
-    //     (the default is 16px, but can be customized by user)
-    // Calculation:
-    // DR% = RRpx/Bpx = 10px/16px = 0.625 = 62.5%
-    // if Bpx = 16px, then DRpx = DR% of Bpx = 62.5% of 16px = 10px
-    // if Bpx = 20px, then DRpx = DR% of Bpx = 62.5% of 20px = 12.5px
-    // if Bpx = 22px, then DRpx = DR% of Bpx = 62.5% of 22px = 13.75px
-}
-
-body {
-    box-sizing: border-box;
-    color: $color-grey-dark;
-    font-family: "Lato", sans-serif;
-    // font-size: 16px;
-    font-weight: 400;
-    line-height: 1.7;
-}
+@import "../src/assets/scss/main.scss";
 
 #nav {
     align-items: flex-start;
