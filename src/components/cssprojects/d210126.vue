58c34f3b4723d3cad3704810a5a0976a2170b33a
igoryen <igor.yentaltsev@gmail.com>
2021-02-11 18:15:47 -0500

    35-C. Added text and images section.

--- src/components/cssprojects/d210126.vue
+++ src/components/cssprojects/d210126.vue
@@ -26,10 +26,15 @@
 
         <main>
             <section class="section-about">
-                <div class="u-center-text">
+                <div class="u-center-text u-margin-bottom-8">
                     <h2 class="heading-secondary">
                         Exciting tours for adventurous people
                     </h2>
+                </div>
+
+                <div class="row">
+                    <div class="col-50prc">Text</div>
+                    <div class="col-50prc">Images</div>
                 </div>
             </section>
         </main>
