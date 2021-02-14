18a7e6809fc875f97431eb8da306fab906b3f8f4
igoryen <igor.yentaltsev@gmail.com>
2021-02-14 14:48:04 -0500

    37-a. The About section. Added images.

--- src/components/cssprojects/d210126.vue
+++ src/components/cssprojects/d210126.vue
@@ -54,9 +54,24 @@
                         </p>
 
                         <a href="#" class="btn-text">Learn more &rarr;</a>
-
                     </div>
-                    <div class="col-50prc">Images</div>
+                    <div class="col-50prc">
+                        <div class="composition">
+                            <img
+                                src="../../assets/images/natours/nat-1-large.jpg"
+                                alt="Photo 1"
+                                class="composition__photo composition__photo--p1"
+                            /><img
+                                src="../../assets/images/natours/nat-2-large.jpg"
+                                alt="Photo 2"
+                                class="composition__photo composition__photo--p2"
+                            /><img
+                                src="../../assets/images/natours/nat-3-large.jpg"
+                                alt="Photo 3"
+                                class="composition__photo composition__photo--p3"
+                            />
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
